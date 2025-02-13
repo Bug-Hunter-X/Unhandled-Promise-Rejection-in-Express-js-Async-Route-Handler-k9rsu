a939cfce20140e092edbe785d00f9387d7d359cf
BugHunter-X <askzenai@gmail.com>
2025-02-13 08:02:09 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res, next) => {
+  someAsyncOperation().then(() => {
+    res.send('Hello World!');
+  }).catch(next);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+async function someAsyncOperation() {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        reject(new Error('Something went wrong!'));
+      } else {
+        resolve();
+      }
+    }, 1000);
+  });
+}
